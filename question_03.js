d13c6a19d90784e1dcaf93db40e9dad8f62174d0
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-13 16:40:44 +0500

    initiall commit

---
+++ question_03.js
@@ -0,0 +1,3 @@
+// Write a class that represents a person with a name and an age. The class should
+// have a constructor that takes the name and age as parameters, and a method that
+// returns the person’s introduction.
